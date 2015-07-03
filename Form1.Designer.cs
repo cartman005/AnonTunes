60e2370e234e5dbf0dde35499155352196ae1c60
Dan Kozlowski <cartman005@gmail.com>
2015-07-02 23:12:48 -0400

    Initial Commit
    AtomicParsley wrapper for removing personal information from iTunes
    Music Store purchases.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AnonTunes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wipeButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pathButton = new System.Windows.Forms.Button();
+            this.pathText = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // wipeButton
+            // 
+            this.wipeButton.Location = new System.Drawing.Point(306, 91);
+            this.wipeButton.Name = "wipeButton";
+            this.wipeButton.Size = new System.Drawing.Size(165, 52);
+            this.wipeButton.TabIndex = 2;
+            this.wipeButton.Text = "Wipe";
+            this.wipeButton.UseVisualStyleBackColor = true;
+            this.wipeButton.Click += new System.EventHandler(this.wipeButton_Click);
+            // 
+            // pathButton
+            // 
+            this.pathButton.Location = new System.Drawing.Point(639, 21);
+            this.pathButton.Name = "pathButton";
+            this.pathButton.Size = new System.Drawing.Size(117, 45);
+            this.pathButton.TabIndex = 1;
+            this.pathButton.Text = "Browse";
+            this.pathButton.UseVisualStyleBackColor = true;
+            this.pathButton.Click += new System.EventHandler(this.pathButton_Click);
+            // 
+            // pathText
+            // 
+            this.pathText.Location = new System.Drawing.Point(12, 21);
+            this.pathText.Name = "pathText";
+            this.pathText.Size = new System.Drawing.Size(621, 45);
+            this.pathText.TabIndex = 1;
+            this.pathText.Click += new System.EventHandler(this.pathLabel_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(24, 72);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(739, 13);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 154);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pathText);
+            this.Controls.Add(this.pathButton);
+            this.Controls.Add(this.wipeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button wipeButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button pathButton;
+        private System.Windows.Forms.Label pathText;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnonTunes
+{
+    public partial class Form1 : Form
+    {
+        private static int TIMEOUT_SECONDS = 30;
+        private static string EXECUTABLE = "AtomicParsley-utf8.exe";
+        private static string ARGUMENTS = "--DeepScan --manualAtomRemove moov.trak.mdia.minf.stbl.stsd.mp4a.pinf --manualAtomRemove moov.trak[2].mdia.minf.stbl.stsd.avc1.pinf --manualAtomRemove moov.trak.mdia.minf.stbl.stsd.mp4a.UUID --manualAtomRemove moov.udta.meta.ilst.apID --manualAtomRemove moov.udta.meta.ilst.sfID --manualAtomRemove moov.udta.meta.ilst.purd --manualAtomRemove moov.udta.Xtra --manualAtomRemove moov.udta.meta.ilst.ownr --freefree 0 -W";
+
+        public Form1()
+        {
+            InitializeComponent();
+            pathText.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+        }
+
+        private void wipeButton_Click(object sender, EventArgs e)
+        {
+            // Extensions of files to wipe
+            var extensionList = new string[] { ".mp4", ".m4a", ".m4b", ".m4r" };
+
+            // Iterate through parent folder to find all .mp4 files
+            var musicFiles = Directory.GetFiles(pathText.Text, "*", SearchOption.AllDirectories)
+                .Where(n => extensionList.Contains(Path.GetExtension(n), StringComparer.OrdinalIgnoreCase))
+                .ToList();
+
+            // Initialize the progress bar
+            progressBar1.Visible = true;
+            progressBar1.Minimum = 1;
+            progressBar1.Maximum = musicFiles.Count;
+            progressBar1.Value = 1;
+            progressBar1.Step = 1;
+
+            // Launch AtomicParsley for each file
+            string output;
+            string arguments;
+            foreach (string filePath in musicFiles)
+            {
+                arguments = "\"" + filePath + "\" " + ARGUMENTS;
+                Debug.WriteLine("Wiping {0}", filePath, "");
+                output = LaunchEXE.Run(EXECUTABLE, arguments, TIMEOUT_SECONDS);
+                //if (output != "")
+                //{
+                    //output = output.Substring(output.LastIndexOf(Environment.NewLine));
+                    Debug.WriteLine(output);
+                //}
+                progressBar1.PerformStep();
+            }
+
+            progressBar1.Visible = false;
+        }
+
+        private void pathButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                this.pathText.Text = folderBrowserDialog1.SelectedPath;                
+        }
+
+        private void pathLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
